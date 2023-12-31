321608efe9665b490caf7fca2922e80f0f459a44
MsftDev221 <aron.kvaszta021@gmail.com>
2023-12-31 10:08:55 +0100

    Add documentation

--- src/main.py
+++ src/main.py
@@ -2,15 +2,39 @@ import random
 
 
 def evaluate(check: int, against: int) -> tuple[str, bool]:
+    """
+    Compare a user-provided number with a generated number and provide feedback.
+
+    Parameters:
+    - check (int): The user-provided number for comparison.
+    - against (int): The generated number to compare against.
+
+    Returns:
+    tuple[str, bool]: A tuple containing two elements:
+        - A string providing feedback on the comparison result.
+        - A boolean indicating the success of the comparison (True if numbers are equal, False otherwise).
+    """
     if (check > against):
-        return ("Your number is greater than the generated one. Try again", False)
+        return ("Your number is too high! Try again.", False)
     elif (check < against):
-        return ("Your number is less than the generated one. Try again.", False)
+        return ("Your number is too low! Try again.", False)
     else:
         return (f"That's it! The generated number was {against}!", True)
 
 
 def get_input(_msg: str) -> int:
+    """
+    Prompt the user for input and ensure the input is a valid integer.
+
+    Parameters:
+    - _msg (str): The message displayed to prompt the user for input.
+
+    Returns:
+    int: The valid integer entered by the user.
+
+    Raises:
+    ValueError: If the user enters a non-integer value.
+    """
     num = input(_msg)
 
     while (not num.isdecimal()):
@@ -20,6 +44,19 @@ def get_input(_msg: str) -> int:
 
 
 def main():
+    """
+    Starting point of guess the computer-generated random number game.
+
+    This script generates a random number between 1 and 100, then prompts the user to guess it.
+    After each guess, the script provides feedback on whether the guess is too high, too low, or correct.
+
+    Usage:
+    1. Run the script.
+    2. Enter your guess when prompted.
+    3. Receive feedback until you correctly guess the generated number.
+
+    Note: This script uses the `get_input` and `evaluate` functions.
+    """
     computer_number: int = random.randint(1, 100)
 
     user_input = get_input(
@@ -33,8 +70,6 @@ def main():
 
         result = evaluate(user_input, computer_number)
         print(result[0])
-
-    return 0
 
 
 if __name__ == "__main__":
